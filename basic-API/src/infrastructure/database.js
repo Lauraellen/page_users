bfc86a0c94b93466d717419229ae7459255e89f3
laura.souza <laura.souza@gclaims.com.br>
2023-06-01 23:11:05 -0300

    ajustes

---
+++ basic-API/src/infrastructure/database.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const uri = `mongodb+srv://root:root123@cluster0.0mkmaiy.mongodb.net/`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    id: {
+        type: String,
+        index: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    nome: String,
+    senha: String,
+});
+
+const UserModel = mongoose.model('UserModel', UserSchema);
+
+module.exports = {
+    UserModel,
+};
